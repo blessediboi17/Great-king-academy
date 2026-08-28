50892de796086725659471734a6561f10c148c51
Blessed iboi <iboiblessed1@gmail.com>
2026-08-28 11:36:01 +0000

    Configure base URL in vite.config.js
    Set the base URL for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://blessediboi17.github.io/great-king-academy.github.io', // Replace with your exact GitHub repository name
+})
